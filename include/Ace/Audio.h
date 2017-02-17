b7cd5d9221841c182ad3786952d5183de5494d3e
KAMIT\petterivai <petteri.vaisanen951@hotmail.com>
2017-02-17 10:42:39 +0200

    Added functions to Audio

--- include/Ace/Audio.h
+++ include/Ace/Audio.h
@@ -1,43 +1,82 @@
 #pragma once
 
+#include <Ace/Vector3.h>
 #include <Ace/File.h>
+
 
 
 #include <memory>
 
 namespace ace
 {
-	class AudioSample
+	class AudioClip
 	{
+		friend class Audio;
 	public:
-		struct AudioSampleImpl;
+		struct AudioClipImpl;
+
 
 	private:
-		std::shared_ptr<AudioSampleImpl> impl;
+		std::shared_ptr<AudioClipImpl> impl;
+		bool ispaused;
+
+		float volume;
+		float pitch;
+		UInt32 priority;
+		bool loop;
+		
 
 	public:
 
-		AudioSample(AudioSampleImpl* sample = nullptr) : impl(sample)
+		
+		AudioClip(AudioClipImpl* clip = nullptr) : impl(clip)
 		{
 
 		}
 
-		AudioSample(const File& file);
+		AudioClip(const File& file, float volume = 1.0f, bool loop = false, UInt32 priority = 10) : volume(volume), loop(loop), priority(priority)
+		{
+
+		}
 	
 
-		inline AudioSampleImpl* operator->() const
+		inline AudioClipImpl* operator->() const
 		{
 			return impl.get();
 		}
+
+		void SetVolume(float volume);
+		float GetVolume();
+		void SetPitch(float pitch);
+		float GetPitch();
+		void SetLoop(bool loop);
+		void SetElapsedTime(double time);
+		double GetElapsedTime();
+		double GetTotalTime();
+		bool IsPlaying();
+		void SetPriority(UInt32);
+		UInt32 GetPriority();
+
+
+		//void SetPosition(math::Vector3 position);
+		//void SetVelocity(math::Vector3 velocity);
 
 	};
 
 	class Audio
 	{
 	public:
+
 		static void Init();
-		static bool Update(const AudioSample& sample);
-		static void PlayAudio(const AudioSample& sample);
+		static void PauseAudio(AudioClip& Clip);
+		static void PauseAudio(AudioClip& Clip, bool ispaused);
+		static bool Update(const AudioClip& Clip);
+		static void PlayAudio(const AudioClip& Clip);
+		//static void PlayAudioAtPosition(const AudioClip& Clip, Vector3 pos);
+		//static void SetAttributes(vector3 apPos, const float* apVel, const float* apForward, const float* apUpward);
+		static void StopAudio(const AudioClip& Clip);
+		static void StopAll();
+		static void SetMasterVolume(float volume);
 
 	private:
 	
--- src/Ace/Audio.cpp
+++ src/Ace/Audio.cpp
@@ -9,23 +9,24 @@
 
 namespace ace
 {
-	struct AudioSample::AudioSampleImpl
+	struct AudioClip::AudioClipImpl
 	{
-		cOAL_Sample* sample;
+		cOAL_Sample* clip;
 		int id;
-		AudioSampleImpl(cOAL_Sample* sample) : sample(sample)
+
+		AudioClipImpl(cOAL_Sample* clip) : clip(clip)
 		{
 
 		}
 	};
 
-	AudioSample::AudioSample(const File& file)
+	AudioClip::AudioClip(const File& file, float volume, bool loop, UInt32 priority)
 	{
 		UInt32 pos = file.Size();
 		UInt8* buffer = new UInt8[pos];
 
 		file.Read(buffer, pos);
-		impl.reset(new AudioSample::AudioSampleImpl(OAL_Sample_LoadFromBuffer(buffer, pos)));
+		impl.reset(new AudioClip::AudioClipImpl(OAL_Sample_LoadFromBuffer(buffer, pos)));
 
 		delete buffer;
 	}
@@ -33,9 +34,6 @@ namespace ace
 
 	void Audio::Init()
 	{
-		printf("Initializing OpenAL.\n");
-		fflush(stdout);
-		OAL_SetupLogging(true, eOAL_LogOutput_File, eOAL_LogVerbose_High);
 		cOAL_Init_Params oal_parms;
 		if (OAL_Init(oal_parms) == false)
 		{
@@ -47,20 +45,18 @@ namespace ace
 		}
 	}
 
-	bool Audio::Update(const AudioSample& sample)
+	bool Audio::Update(const AudioClip& clip)
 	{
-		return OAL_Source_IsPlaying(sample->id);
+		return OAL_Source_IsPlaying(clip->id);
 	}
 
-	void Audio::PlayAudio(const AudioSample& sample)
+	void Audio::PlayAudio(const AudioClip& clip)
 	{
-		if (sample->sample)
+		if (clip->clip)
 		{
-			printf("Playing Sample...\n");
-			printf("Channels : %d\nFrequency : %d", sample->sample->GetChannels(), sample->sample->GetFrequency());
-			OAL_Source_Stop(OAL_ALL);
-			sample->id = OAL_Sample_Play(OAL_FREE, sample->sample, 1.0f, true, 10);
-			OAL_Source_SetPaused(sample->id, false);
+			printf("Channels : %d\nFrequency : %d", clip->clip->GetChannels(), clip->clip->GetFrequency());
+			clip->id = OAL_Sample_Play(OAL_FREE, clip->clip, clip.volume, clip.loop, clip.priority);
+			OAL_Source_SetPaused(clip->id, false);
 
 		}
 		else
@@ -68,4 +64,107 @@ namespace ace
 			printf("Error!\n");
 		}
 	}
+
+	void Audio::PauseAudio(AudioClip& clip)
+	{
+		clip.ispaused = !clip.ispaused;
+		PauseAudio(clip, clip.ispaused);
+	}
+
+	void Audio::PauseAudio(AudioClip& clip, bool pause)
+	{
+		OAL_Source_SetPaused(clip->id, pause);
+
+	}
+
+	void Audio::StopAudio(const AudioClip& clip)
+	{
+		OAL_Source_Stop(clip->id);
+	}
+
+	void Audio::StopAll()
+	{
+		OAL_Source_Stop(OAL_ALL);
+	}
+
+	void Audio::SetMasterVolume(float volume)
+	{
+		OAL_Listener_SetMasterVolume(volume);
+	}
+
+	void AudioClip::SetVolume(float volume)
+	{
+		OAL_Source_SetGain((*this)->id, volume);
+	}
+	
+	float AudioClip::GetVolume()
+	{
+		return OAL_Source_GetGain((*this)->id);
+	}
+
+	void AudioClip::SetPitch(float pitch)
+	{
+		OAL_Source_SetPitch((*this)->id, pitch);
+	}
+
+	float AudioClip::GetPitch()
+	{
+		return OAL_Source_GetPitch((*this)->id);
+	}
+
+	void AudioClip::SetLoop(bool loop)
+	{
+		OAL_Source_SetLoop((*this)->id, loop);
+	}
+
+	void AudioClip::SetPriority(UInt32 priority)
+	{
+		OAL_Source_SetPriority((*this)->id, priority);
+	}
+
+	UInt32 AudioClip::GetPriority()
+	{
+		return OAL_Source_GetPriority((*this)->id);
+	}
+
+	void AudioClip::SetElapsedTime(double time)
+	{
+		OAL_Source_SetElapsedTime((*this)->id, time);
+	}
+
+	double AudioClip::GetElapsedTime()
+	{
+		return OAL_Source_GetElapsedTime((*this)->id);
+	}
+
+	double AudioClip::GetTotalTime()
+	{
+		return OAL_Source_GetTotalTime((*this)->id);
+	}
+
+	bool AudioClip::IsPlaying()
+	{
+		return OAL_Source_IsPlaying((*this)->id);
+	}
+
+	//void AudioClip::SetPosition(math::Vector3 position)
+	//{
+	//	OAL_Source_SetPosition((*this)->, position);
+	//}
+
+	//void AudioClip::SetVelocity(math::Vector3 velocity)
+	//{
+	//	OAL_Source_SetPosition((*this)->, velocity);
+	//}
+
+	//void Audio::PlayAudioAtPosition(const AudioClip& Clip, Vector3 pos)
+	//{
+	//	OAL_Source_SetPosition(clip->id, pos);
+	//}
+
+	//void Audio::SetAttributes(vector3 apPos, vector3 apVel, vector3 apForward, vector3 apUpward)
+	//{
+	//	OAL_Listener_SetAttributes(apPos, apVel, apForward, apUpward);
+	//}
+
 }
