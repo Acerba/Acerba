b669a8cc10721ebd0412991525d5d9075a2e6468
Atte Vuorinen <AtteVuorinen@gmail.com>
2017-11-22 14:50:39 +0200

    Updated DemoActivity.java, added navigation hiding.

--- android_template/src/acerba/demo/DemoActivity.java
+++ android_template/src/acerba/demo/DemoActivity.java
@@ -1,7 +1,27 @@
 package org.acerba.demo;
 
+import android.app.*;
+import android.content.*;
+import android.text.InputType;
+import android.view.*;
+
 import org.libsdl.app.SDLActivity;
 
 public class DemoActivity extends SDLActivity {
-
+	
+	@Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+		
+		  if (hasFocus) {
+			  getWindow().getDecorView().setSystemUiVisibility(
+				  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				  | View.SYSTEM_UI_FLAG_FULLSCREEN
+				  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		  }
+    }
+	
 }
